abc9296d083c391c1f5b756886540e83ba8ab96d
MoeedCodes <mmoon8941@gmail.com>
2026-08-31 18:21:58 +0500

    Update Home component to fetch posts on mount
    Refactor useEffect to fetch posts from appwriteService.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,12 +3,14 @@ import appwriteService from "../appwrite/config"
 import { Container,PostCard } from '../components'
 function Home() {
    const [posts,setPosts]=useState([])
-    useEffect(()=>{},[])
-    appwriteService.getPosts([]).then((posts)=>{
+    useEffect(()=>{
+       appwriteService.getPosts([]).then((posts)=>{
         if(posts){
         setPosts(posts.documents)
         }
     })
+    },[])
+    
     if (posts.length===0){
        return (
             <div className="w-full py-8 mt-4 text-center">
