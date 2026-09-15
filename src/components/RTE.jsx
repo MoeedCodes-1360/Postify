9fcb189c8d0d1dfa2c5506071b4f52bb6a5696f6
MoeedCodes <mmoon8941@gmail.com>
2026-09-15 10:49:40 +0500

    Remove console log for tinyMCEApiKey
    Remove console log for API key in RTE component.

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -4,7 +4,7 @@ import { Controller } from "react-hook-form";
 import conf from "../conf/conf";
 
 const RTE = ({ name, control, label, defaultValue = "" }) => {
-                   console.log(conf.tinyMCEApiKey)
+                   
   return (
     <div className="w-full">
       {label && <label className="inline-block mb-1 pl-1">{label}</label>}
