aaf9baaac732713ed5a6b92fcd964535901d909e
MoeedCodes <mmoon8941@gmail.com>
2026-09-15 10:19:54 +0500

    Remove duplicate console log in RTE component
    Removed duplicate console log statement for tinyMCEApiKey.

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -4,6 +4,7 @@ import { Controller } from "react-hook-form";
 import conf from "../conf/conf";
 
 const RTE = ({ name, control, label, defaultValue = "" }) => {
+                   console.log(conf.tinyMCEApiKey)
   return (
     <div className="w-full">
       {label && <label className="inline-block mb-1 pl-1">{label}</label>}
@@ -13,7 +14,6 @@ const RTE = ({ name, control, label, defaultValue = "" }) => {
         render={({ field: { onChange } }) => {
           return <Editor
             apiKey={conf.tinyMCEApiKey}
-                   {console.log(conf.tinyMCEApiKey)}
             initialValue={defaultValue}
             init={{
               height: 500,
